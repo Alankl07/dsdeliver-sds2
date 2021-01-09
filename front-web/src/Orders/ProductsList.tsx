d67b92f646cb974d4284059de8f8c44f36d6b45c
alanNunes <alan.fsa.ans@gmail.com>
2021-01-09 00:19:27 -0300

    Finish front-web

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,18 +1,21 @@
+import { checkIsSelected } from "./helpers";
 import ProductCard from "./ProductCard"
 import { Product } from "./types";
 
 type Props = {
     products: Product[];
+    selectedProducts: Product[];
+    onSelectProduct: (product: Product) => void;
 }
 
-const ProductList = ( { products }: Props ) => {
+const ProductList = ( { products, onSelectProduct, selectedProducts }: Props ) => {
     return(
         <div className="orders-list-container" >
             <div className="orders-list-items" >
                {
                    products.map(product => {
                        return(
-                        <ProductCard key={product.id} 
+                        <ProductCard isSelected={checkIsSelected(selectedProducts, product)} onSelectProduct={onSelectProduct} key={product.id} 
                             product={product}
                         />
                        )
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,16 +1,23 @@
 import './styles.css';
 import { ReactComponent as Logo } from '../assets/logo.svg';
+import { toast } from 'react-toastify';
 import StepHeader from './StepsHeader';
 import ProductList from './ProductsList';
 import { useEffect, useState } from 'react';
 import { OrderLocationData, Product } from './types';
-import { fetchProducts } from '../service/api';
+import { fetchProducts, saveOrder } from '../service/api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 const Order = () => {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
-
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price
+    },0)
     console.log(products)
     useEffect(() => {
         fetchProducts().then(response => {
@@ -20,12 +27,42 @@ const Order = () => {
         })
     },[])
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
+      const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+          ...orderLocation!,
+          products: productsIds
+        }
+      
+        saveOrder(payload).then((response) => {
+          toast.error(`Pedido enviado com sucesso! Nº${response.data.id}`);
+          setSelectedProducts([]);
+        })
+          .catch(() => {
+            toast.warning('Erro ao enviar pedido');
+          })
+      }
+
     return(
+        <>
         <div className="orders-container">
             <StepHeader/>
-            <ProductList products={products} />
+            <ProductList selectedProducts={selectedProducts} onSelectProduct={handleSelectProduct} products={products} />
             <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+            <OrderSummary onSubmit={handleSubmit} amount={selectedProducts.length} totalPrice={totalPrice} />
         </div>
+        <Footer/>
+        </>
     )
 }
 
